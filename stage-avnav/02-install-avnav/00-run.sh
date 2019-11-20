abeb11bd1c4e3125be72c72dcaa96b7fbc51d269
free-x <oroitburd@gmail.com>
2019-11-20 20:26:18 +0100

    Fix dpkg

--- stage-avnav/02-install-avnav/00-run.sh
+++ stage-avnav/02-install-avnav/00-run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
-on_chroot dpkg -i files/avnav_20191112_all.deb
-on_chroot dpkg -i files/avnav-raspi_20191027_all.deb
+install -m 644 files/avnav_20191112_all.deb "${ROOTFS_DIR}/tmp"
+install -m 644 files/avnav-raspi_20191027_all.deb "${ROOTFS_DIR}/tmp"
+
+on_chroot << EOF
+dpkg -i /tmp/avnav_20191112_all.deb
+dpkg -i /tmp/avnav-raspi_20191027_all.deb
+EOF
 
