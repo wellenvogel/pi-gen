a42f9ffc660be69a9bb96054e537f8fe1389e0a2
free-x <oroitburd@gmail.com>
2019-11-22 12:40:32 +0100

    Fix hostapd

--- stage-avnav/02-install-avnav/00-run.sh
+++ stage-avnav/02-install-avnav/00-run.sh
@@ -6,5 +6,7 @@ install -m 644 files/avnav-raspi_20191027_all.deb "${ROOTFS_DIR}/tmp"
 on_chroot << EOF
 dpkg -i /tmp/avnav_20191112_all.deb
 dpkg -i /tmp/avnav-raspi_20191027_all.deb
+systemctl daemon-reload
+systemctl disable hostapd.service
 EOF
 
