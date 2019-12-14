458bab2b397cf39eecc061f8c71c6084e586212f
free-x <oroitburd@gmail.com>
2019-12-14 15:55:37 +0100

    Fix run

--- stage-avnav/02-install-avnav/00-run.sh
+++ stage-avnav/02-install-avnav/00-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-install -m 644 files/avnav_20191129_all.deb "${ROOTFS_DIR}/tmp"
-install -m 644 files/avnav-raspi_20191129_all.deb "${ROOTFS_DIR}/tmp"
+install -m 644 files/avnav_20191213_all.deb "${ROOTFS_DIR}/tmp"
+install -m 644 files/avnav-raspi_20191213_all.deb "${ROOTFS_DIR}/tmp"
 
 on_chroot << EOF
-dpkg -i /tmp/avnav_20191129_all.deb
-dpkg -i /tmp/avnav-raspi_20191129_all.deb
-rm -f /tmp/avnav_20191129_all.deb /tmp/avnav-raspi_20191129_all.deb
+dpkg -i /tmp/avnav_20191213_all.deb
+dpkg -i /tmp/avnav-raspi_20191213_all.deb
+rm -f /tmp/avnav_20191213_all.deb /tmp/avnav-raspi_20191213_all.deb
 systemctl daemon-reload
 systemctl disable hostapd.service
 systemctl disable gpsd.socket gpsd.service
