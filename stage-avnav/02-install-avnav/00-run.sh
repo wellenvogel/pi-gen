0632915d5f8305b30b1cfd7cb2ec28ed9416153e
free-x <oroitburd@gmail.com>
2019-12-17 07:58:03 +0100

    Daily 20191216, disable ntp, cosmetic

--- stage-avnav/02-install-avnav/00-run.sh
+++ stage-avnav/02-install-avnav/00-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
-install -m 644 files/avnav_20191213_all.deb "${ROOTFS_DIR}/tmp"
-install -m 644 files/avnav-raspi_20191213_all.deb "${ROOTFS_DIR}/tmp"
+install -m 644 files/avnav_20191216_all.deb "${ROOTFS_DIR}/tmp"
+install -m 644 files/avnav-raspi_20191216_all.deb "${ROOTFS_DIR}/tmp"
 
 on_chroot << EOF
-dpkg -i /tmp/avnav_20191213_all.deb
-dpkg -i /tmp/avnav-raspi_20191213_all.deb
-rm -f /tmp/avnav_20191213_all.deb /tmp/avnav-raspi_20191213_all.deb
+dpkg -i /tmp/avnav_20191216_all.deb
+dpkg -i /tmp/avnav-raspi_20191216_all.deb
+rm -f /tmp/avnav_20191216_all.deb /tmp/avnav-raspi_20191216_all.deb
 systemctl daemon-reload
 systemctl disable hostapd.service
 systemctl disable gpsd.socket gpsd.service
+systemctl disable ntp.service
+cat files/figlet >> /etc/motd
 EOF
 
