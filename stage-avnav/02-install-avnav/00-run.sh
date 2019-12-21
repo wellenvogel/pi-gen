b2752a9e9bc4edb0b765b0b6a6a3e7c98eeec8cb
free-x <oroitburd@gmail.com>
2019-12-21 09:09:35 +0100

    Fix wpa_supplicant issue

--- stage-avnav/02-install-avnav/00-run.sh
+++ stage-avnav/02-install-avnav/00-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
 cat files/figlet >> ${ROOTFS_DIR}/etc/motd
+cp files/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/
 
 on_chroot << EOF
 systemctl daemon-reload
 systemctl disable hostapd.service
 systemctl disable gpsd.socket gpsd.service
 systemctl disable ntp.service
+apt install -y avnav avnav-raspi
 EOF
 
