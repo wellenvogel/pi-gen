11e97d4899dd7adf47c077f52c52055ee305bc06
free-x <oroitburd@gmail.com>
2020-01-21 22:02:17 +0100

    Add example for Can-Hat

--- stage-avnav/04-customizing/01-run.sh
+++ stage-avnav/04-customizing/01-run.sh
@@ -3,4 +3,6 @@
 on_chroot << EOF
 echo "set mouse-=a" >> /home/pi/.vimrc
 echo "set mouse-=a" >> /root/.vimrc
+echo "#dtparam=spi=on" >> /boot/config.txt
+echo "#dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=25,spimaxfrequency=1000000" >> /boot/config.txt
 EOF
