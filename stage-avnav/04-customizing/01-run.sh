5120c58fca8dce7d4a767c34aaab4c67035e59d8
free-x <oroitburd@gmail.com>
2020-02-21 15:14:12 +0100

    Add uart_control utility

--- stage-avnav/04-customizing/01-run.sh
+++ stage-avnav/04-customizing/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+install -m 775 -o 1000 -g 1000 files/uart_control "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
 on_chroot << EOF
 echo "set mouse-=a" >> /home/pi/.vimrc
 echo "set mouse-=a" >> /root/.vimrc
