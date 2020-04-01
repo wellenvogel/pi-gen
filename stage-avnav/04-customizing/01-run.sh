addeb2d3dd0af288dc1ee10bfed692b0d8ef9dea
free-x <oroitburd@gmail.com>
2020-04-01 11:56:42 +0200

    avoid rfkill

--- stage-avnav/04-customizing/01-run.sh
+++ stage-avnav/04-customizing/01-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-install -m 775 -o 1000 -g 1000 files/uart_control "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+install -v -m 775 -o 1000 -g 1000 files/uart_control "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
+install -v -m 755 -o 0 -g 0 files/rc.local "${ROOTFS_DIR}/etc/"
 
 on_chroot << EOF
 echo "set mouse-=a" >> /home/pi/.vimrc
