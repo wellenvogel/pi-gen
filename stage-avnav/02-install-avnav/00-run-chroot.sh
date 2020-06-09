c6ea16ddb4aa723c2c4e62c069c8c03533dc2809
free-x <oroitburd@gmail.com>
2020-06-09 10:10:08 +0200

    dpkg -> apt

--- stage-avnav/02-install-avnav/00-run-chroot.sh
+++ stage-avnav/02-install-avnav/00-run-chroot.sh
@@ -9,7 +9,7 @@ else
 	base="https://www.wellenvogel.de/software/avnav/downloads/daily/latest"
 	curl -k -o /tmp/avnav.deb  "$base/avnav_latest_all.deb" || exit 1
 	curl -k -o /tmp/avnav-raspi.deb "$base/avnav-raspi_latest_all.deb" || exit 1
-	dpkg -i /tmp/avnav.deb
-	dpkg -i /tmp/avnav-raspi.deb
+	apt install -y /tmp/avnav.deb
+	apt install -y /tmp/avnav-raspi.deb
 fi
 
