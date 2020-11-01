d45528d410f72971ee42c4b3fbb6e5a7e9a32261
andreas <andreas@wellenvogel.de>
2020-11-01 19:14:58 +0100

    install ocharts provider from repo even if AVNAV_DAILY is set

--- stage-avnav/02-install-avnav/00-run-chroot.sh
+++ stage-avnav/02-install-avnav/00-run-chroot.sh
@@ -4,7 +4,7 @@ systemctl disable hostapd.service
 systemctl disable gpsd.socket gpsd.service
 systemctl disable ntp.service
 if [ "$AVNAV_DAILY" != "1" ] ; then
-	apt install -y avnav avnav-raspi avnav-oesenc avnav-ocharts-plugin
+	apt install -y avnav avnav-raspi 
 else
 	base="https://www.wellenvogel.de/software/avnav/downloads/daily/latest"
 	curl -k -o /tmp/avnav.deb  "$base/avnav_latest_all.deb" || exit 1
@@ -12,4 +12,5 @@ else
 	apt install -y /tmp/avnav.deb
 	apt install -y /tmp/avnav-raspi.deb
 fi
+apt install -y avnav-oesenc avnav-ocharts-plugin
 
