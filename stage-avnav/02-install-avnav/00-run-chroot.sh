8671635ae0f04d47ec8cf5f6acd97b12a823407d
free-x <oroitburd@gmail.com>
2021-01-26 20:35:15 +0100

    Add avnav-update-plugin to install

--- stage-avnav/02-install-avnav/00-run-chroot.sh
+++ stage-avnav/02-install-avnav/00-run-chroot.sh
@@ -12,5 +12,5 @@ else
 	apt install -y /tmp/avnav.deb
 	apt install -y /tmp/avnav-raspi.deb
 fi
-apt install -y avnav-oesenc avnav-ocharts-plugin avnav-history-plugin
+apt install -y avnav-oesenc avnav-ocharts-plugin avnav-history-plugin avnav-update-plugin
 
